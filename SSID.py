db0176c5598d9e790a449ab5ec641323e1bfc80e
Aaron <asvaaron@gmail.com>
2016-06-28 10:21:44 -0600

    lalalala

--- SSID.py
+++ SSID.py
@@ -1,12 +1,12 @@
 
 class SSID:
 
-    def __init__(self,ssid='',strenght=0.0,upspeed=0.0,dospeed=0.0):
+    def __init__(self,ssid='',strenght=0.0,upspeed=0.0,dospeed=0.0,count=1):
         self._ssid=ssid
         self._upspeed=upspeed
         self._dospeed=dospeed
         self._strenght=strenght
-        self.count=0
+        self._count=count
 
 
     def getUpspeed(self):
@@ -21,8 +21,8 @@ class SSID:
         self._strenght+=other.getStrenght()
         self._upspeed+=other.getUpspeed()
         self._dospeed+=other.getDospeed()
-        self.count+=1
-        return SSID(self._ssid,self._strenght,self._upspeed,self._dospeed)
+        self._count+=1
+        return SSID(self._ssid,self._strenght,self._upspeed,self._dospeed,self._count)
 
 
     def __str__(self):
@@ -30,13 +30,18 @@ class SSID:
         st += '  Strenght: ' + str(self._strenght)
         st += '  Upload Speed: ' + str(self._upspeed)
         st += '  Download Speed: ' + str(self._dospeed)
+        st += '  Count: ' + str(self._count)
+
         return st
+
+    def averages(self):
+        return (self._strenght/self._count,self._dospeed/self._count,self._upspeed/self._count)
 
 if __name__ == '__main__':
     S1=SSID('lala',2,3,4)
-    S2=SSID('lwle',1.22,3.3)
+    S2=SSID('lwle',1.22,3.3,1)
 
-    print(S1+S2)
+    print(S1+S1+S2+S2)
 
 
 
